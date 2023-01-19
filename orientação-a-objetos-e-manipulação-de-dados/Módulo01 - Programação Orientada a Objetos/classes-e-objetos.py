e981962f35039e08d1e98aa8aae638637bb62981
André Victor <86532340+AndreV1ctor@users.noreply.github.com>
2023-01-19 15:47:37 -0300

    up

--- orientação-a-objetos-e-manipulação-de-dados/Módulo01 - Programação Orientada a Objetos/classes-e-objetos.py
+++ orientação-a-objetos-e-manipulação-de-dados/Módulo01 - Programação Orientada a Objetos/classes-e-objetos.py
@@ -1,11 +1,13 @@
+#classes - montar os objetos (aproximar real das estruturas do mundo digital)
+#A função __init__, também chamada de"método construtor", é o construtor da classe. Responsável por toda vez que criar um novo objeto da classe (Veiculo), dizer o que precisamos pra construir a classe.
 class Veiculo():
-    def __init__(self, tipo, chassi, marca, modelo, ano):
+   def __init__(self, tipo, chassi, marca, modelo, ano):
         self.tipo = tipo
         self.chassi = chassi
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
-
+    
 class Aviao():
     def __init__(self, tipo, motor, linha_aerea, modelo, ano):
         self.tipo = tipo
@@ -13,8 +15,6 @@ class Aviao():
         self.linha_aerea = linha_aerea
         self.modelo = modelo
         self.ano = ano
-
-carro = Veiculo('carro', '1515198987456', 'MarcaX', 'X001', '2022')
-print(vars(carro))
-objetos_aviao = Aviao('Carga', 'Quadrimotor', 'SoulCode Airlines', 'Airbus a380', '2020')
-print(vars(objetos_aviao))
+        
+carro = Veiculo('carro', '4f8sd94f9sd84f98sd4f', 'MarcaX', 'X001', '2012')
+objeto_aviao = Aviao('Carga', 'Quadrimotor', 'SoulCode Airlines', 'Airbus a380', '2010')
